10c69d08b45910123db821dac06765e81c0397a9
05alves <117731310+05alves@users.noreply.github.com>
2022-11-16 11:04:22 -0300

    soma

---
+++ soma.r
@@ -0,0 +1,24 @@
+Algoritmo "semnome"
+// Disciplina   : [Linguagem e Lógica de Programação]
+// Professor   : Antonio Carlos Nicolodi 
+// Descrição   : Aqui você descreve o que o programa faz! (função)
+// Autor(a)    : Nome do(a) aluno(a)
+// Data atual  : 16/11/2022
+Var
+// Seção de Declarações das variáveis 
+a, b: real
+
+Inicio
+// Seção de Comandos, procedimento, funções, operadores, etc... 
+a <- 10
+b <-5
+escreva("Soma de a + b: ")
+escreval(a+b)
+escreva("Soma de a - b: ")
+escreval(a-b)
+escreva("Soma de a * b: ")
+escreval(a*b)
+escreva("Soma de a / b: ")
+escreval(a/b)
+
+Fimalgoritmo

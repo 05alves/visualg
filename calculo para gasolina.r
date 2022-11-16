0c35df6298fa65879620805444fa4d259985c6ba
05alves <117731310+05alves@users.noreply.github.com>
2022-11-16 11:33:55 -0300

    Calculo de gasolina

---
+++ calculo para gasolina.r
@@ -0,0 +1,22 @@
+Algoritmo "semnome"
+// Disciplina   : [Linguagem e Lógica de Programação]
+// Professor   : Antonio Carlos Nicolodi 
+// Descrição   : Aqui você descreve o que o programa faz! (função)
+// Autor(a)    : Nome do(a) aluno(a)
+// Data atual  : 16/11/2022
+Var
+// Seção de Declarações das variáveis 
+combustivel: real
+distanciakm: real
+litros: real
+
+Inicio
+// Seção de Comandos, procedimento, funções, operadores, etc... 
+escreval("calcular o combustivel gasto.")
+escreval("a distancia/km sendo 2000, com 10 litros.")
+distanciakm <- 2000
+litros <- 10
+combustivel <- distanciakm * litros
+escreval("sendo assim a quantidade de combustivel gasto será:",combustivel, "L/km" )
+
+Fimalgoritmo
